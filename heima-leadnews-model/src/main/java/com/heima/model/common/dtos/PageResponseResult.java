a08f2cef6729240d282b76e4439475c973e974a7
zhiyuan <zhiyuantop@163.com>
2022-06-04 00:58:27 +0800

    添加通用请求和返回

---
+++ heima-leadnews-model/src/main/java/com/heima/model/common/dtos/PageResponseResult.java
@@ -0,0 +1,40 @@
+package com.heima.model.common.dtos;
+
+/**
+ * 分页通用返回
+ */
+public class PageResponseResult extends ResponseResult {
+    private Integer currentPage;
+    private Integer size;
+    private Integer total;
+
+    public PageResponseResult(Integer currentPage, Integer size, Integer total) {
+        this.currentPage = currentPage;
+        this.size = size;
+        this.total = total;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+}
